6e4065ae491e6bb3a51f7f024cd126b83a8e6db7
Dāvis Fjodorovs <daavisff@gmail.com>
2023-01-28 13:03:10 +0200

    Created completed tasks section in html, created ability to add a task to task lsit, created css file for later styling

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
-let inputField = document.getElementById('inputField');
+let input = document.getElementById('inputField');
 let addTaskButton = document.getElementById('addTask');
-let taskContainer = document.getElementById('taskList');
+let taskList = document.getElementById('taskList');
+let completedList = document.getElementById('completedList');
 
 addTaskButton.addEventListener('click', () => {
-    let task = document.createElement('p');
-    task.innerText = inputField.value;
-    taskContainer.appendChild(task);
-    inputField.value = '';
+    let task = document.createElement('div');
+    let completeButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
+    let taskText = document.createElement('p');
+
+    taskText.innerText = input.value;
+    completeButton.innerText = 'Complete';
+    deleteButton.innerText = 'Delete';
+
+    task.appendChild(taskText);
+    task.appendChild(completeButton);
+    task.appendChild(deleteButton);
+
+    taskList.appendChild(task);
+
+    input.value = '';
 });
+
+// Ability to edit task
+
+// Ability to delete task
+
+// Ability to complete task
+
+// Ability to add and complete edit with enter press
+
+// Add a completed list
